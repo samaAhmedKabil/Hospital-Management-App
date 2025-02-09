bba8db44aad472975c4bc5a4bbd04e9447c74532
Sama <sama.kabil2004@gmail.com>
2025-02-09 21:00:41 +0200

    V1.1

--- app/src/main/java/com/example/hospitalapplication/ui/commonUsage/attend/AttendanceFragment.kt
+++ app/src/main/java/com/example/hospitalapplication/ui/commonUsage/attend/AttendanceFragment.kt
@@ -1,8 +1,35 @@
 package com.example.hospitalapplication.ui.commonUsage.attend
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.hospitalapplication.R
+import com.example.hospitalapplication.databinding.FragmentAttendanceBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AttendanceFragment:Fragment() {
+    private var _binding  : FragmentAttendanceBinding?= null
+    private val binding get() = _binding!!
+    private val viewModel: AttendanceViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_attendance , container , false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentAttendanceBinding.bind(view)
+        backArrowClick()
+    }
+
+    private fun backArrowClick(){
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
